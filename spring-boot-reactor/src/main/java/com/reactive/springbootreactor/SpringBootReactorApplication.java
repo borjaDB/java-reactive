eba15c3531740685edd2c60ea99a3303fbae9088
borjadb <borja881@outlook.com>
2024-02-13 19:45:21 +0100

    Flatmap method created

--- spring-boot-reactor/src/main/java/com/reactive/springbootreactor/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/reactive/springbootreactor/SpringBootReactorApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +24,38 @@ public class SpringBootReactorApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         log.info("### Starting Spring ###");
+        this.flatMapSample();
 
+    }
+
+    public void flatMapSample(){
+        List<String> userList = new ArrayList<String>();
+        userList.add("John Rambo");
+        userList.add("Mary Popins");
+        userList.add("Paul Montana");
+        userList.add("Sofia Vergara");
+
+        // Flux is the publisher.
+        Flux.fromIterable(userList)
+                .map(name -> new User(name.split(" ")[0].toUpperCase(), name.split(" ")[1].toUpperCase()))
+                .flatMap(user-> {
+                    if (user.getName().equalsIgnoreCase("sofia")) {
+                        return Mono.just(user);
+                    } else {
+                        return Mono.empty();
+                    }
+
+                })
+                .map(user -> {
+                    String name = user.getName().toLowerCase();
+                    user.setName(name);
+                    return user;
+                })
+                // Creates a subscription and it shows the names. This is an observer and is executed at the same time of the doOnNext.
+                .subscribe(element -> log.info("subscribe method --> " + element));
+    }
+
+    public void iteratorSample(){
         List<String> userList = new ArrayList<String>();
         userList.add("John Rambo");
         userList.add("Mary Popins");
@@ -33,9 +65,9 @@ public class SpringBootReactorApplication implements CommandLineRunner {
         // Flux is the publisher.
         // Flux<String> names = Flux.just("John Rambo", "Mary Popings", "Paul Montana", "Sofia Vergara");
         Flux<String> names = Flux.fromIterable(userList);
-                // Transforms the names and returns a new instance without modifying the original
-                // "Flux<String> names" is immutable
-                // Map always returns a value
+        // Transforms the names and returns a new instance without modifying the original
+        // "Flux<String> names" is immutable
+        // Map always returns a value
         Flux<User>users = names.map(name -> new User(name.split(" ")[0].toUpperCase(), name.split(" ")[1]))
                 .filter(name-> {
                     return name.getName().toLowerCase().equals("sofia");
@@ -71,4 +103,5 @@ public class SpringBootReactorApplication implements CommandLineRunner {
                 }
         );
     }
+
 }
